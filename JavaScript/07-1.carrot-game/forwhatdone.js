9f4bc5361c251e8f96e50278c3f1914afef01fec
forwhatdone <58352811+forwhatdone@users.noreply.github.com>
2021-02-23 20:18:44 +0900

    Update forwhatdone.js

--- JavaScript/07-1.carrot-game/forwhatdone.js
+++ JavaScript/07-1.carrot-game/forwhatdone.js
@@ -1,1 +1,81 @@
-console.log('Hello World');
+'use strict';
+
+const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+
+const field = document.querySelector('.game__field');
+const fieldRect = field.getBoundingClientRect();
+const gameBtn = document.querySelector('.game__button');
+const gameTimer = document.querySelector('.game__timer');
+const gameScore = document.querySelector('.game__score'); 
+
+let started = false; //게임의 상태를 담을 변수
+let score = 0; //최종적은 점수를 담을 변수
+let timer = undefined; //타이머
+
+gameBtn.addEventListener('click', () => {
+    if (started) {
+        stopGame();
+    } else {
+        startGame();
+    }
+    started = !started;
+});
+
+function startGame() {
+    initGame();
+    showStopButton();
+    showTimerAndScore();
+}
+
+function stopGame() {
+    showPlayButton();
+}
+
+function showStopButton() {
+    const icon = gameBtn.querySelector('.fa-play');
+    icon.classList.add('fa-stop');
+    icon.classList.remove('fa-play');
+}
+
+function showTimerAndScore() {
+    gameTimer.style.visibility = 'visible';
+    gameScore.style.visibility = 'visible';
+}
+
+function showPlayButton() {
+    const icon2 = gameBtn.querySelector('.fa-stop');
+    icon2.classList.add('fa-play');
+    icon2.classList.remove('fa-stop');
+}
+
+function initGame() {
+    field.innerHTML = '';
+    gameScore.innerText = '0';
+    // 벌레와 당근을  생선한 뒤 field에 추가해줌
+    addItem('carrot', CARROT_COUNT, 'img/carrot.png');
+    addItem('bug', BUG_COUNT, 'img/bug.png');
+}
+
+function addItem(className, count, imgPath) {
+    const x1 = 0;
+    const y1 = 0;
+    const x2 = fieldRect.width - CARROT_SIZE;
+    const y2 = fieldRect.height - CARROT_SIZE;
+    for (let i = 0; i < count ; i++) {
+        const item = document.createElement('img');
+        item.setAttribute('class', className);
+        item.setAttribute('src', imgPath);
+        item.style.position = 'absolute';
+        const x = randomNumber(x1, x2);
+        const y = randomNumber(y1, y2);
+        item.style.left = `${x}px`;
+        item.style.top = `${y}px`;
+        field.appendChild(item);
+    }
+}
+
+function randomNumber(min, max) {
+    return Math.random() * (max - min) + min; 
+}
